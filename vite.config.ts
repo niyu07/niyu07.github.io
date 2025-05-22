30ed6932e55acafcf63fec4a54c574833ce5ab24
niyu07 <niyu98194@gmail.com>
2025-05-23 00:22:13 +0900

    Add Vite configuration for React project

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  base: "niyu07.github.io",
+  test: {
+    globals: true,
+    environment: "happy-dom",
+    setupFiles: ["./src"],
+  },
+});
